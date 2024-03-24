fdac2f54e739789a071bdbb59e2f933ac60c3597
abhay-raj19 <rajpootabhay423@gmail.com>
2024-03-24 15:21:21 +0530

    Feat:Recoil

--- apps/user-app/api/user/route.ts
+++
@@ -1,3 +0,0 @@
-export async function GET() {
-    return Response.json({ username: "abhay", email: "raj@gmail.com" })
-  }
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,16 +1,10 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
-import { PrismaClient } from "@repo"
+"use client";
 
+import { useBalance } from "@repo/store/useBalance";
 
-
-export default function Page(): JSX.Element {
-  return (
-    <div className="text-xl">
-      hi there from user-app
-    </div>
-  );
+export default function() {
+  const balance = useBalance();
+  return <div className="flex justify-center items-center h-screen">
+    hi there {balance}
+  </div>
 }
